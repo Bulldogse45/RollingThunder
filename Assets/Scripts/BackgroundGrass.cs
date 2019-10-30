632ce15dc9542ee260132578cc10c67feb26e7e5
Matthew Fair <matt.j.fair@gmail.com>
2019-10-30 00:31:42 -0400

    add scrolling and widen game area

---
+++ Assets/Scripts/BackgroundGrass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundGrass : MonoBehaviour
+{
+    float fallSpeed = -4f;
+    Vector2 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Mathf.Repeat (Time.time * fallSpeed, 10f);
+        transform.position = startPosition + Vector2.down * newPosition;
+    }
+}
