1523249e2b584f114c4c18eef0447035791164a6
Matthew Fair <matt.j.fair@gmail.com>
2019-11-01 13:37:58 -0400

    Add run up and down and speed change

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,17 +8,17 @@ public class Ball : MonoBehaviour
     {
         Rigidbody2D rb2D = GetComponent<Rigidbody2D>();
         if (Input.GetKey(KeyCode.A)){
-            rb2D.velocity = new Vector2(-15f, 0f);
+            rb2D.velocity = new Vector2(-12f, 0f);
         }
         if (Input.GetKey(KeyCode.D)){
             print("This is a test");
             rb2D.velocity = new Vector2(15f, 0f);
         }
         if (Input.GetKey(KeyCode.W)){
-            rb2D.velocity = new Vector2(0f, 15f);
+            rb2D.velocity = new Vector2(0f, 7.5f);
         }
         if (Input.GetKey(KeyCode.S)){
-            rb2D.velocity = new Vector2(0f, -15f);
+            rb2D.velocity = new Vector2(0f, -12f);
         }
     }
     void OnCollisionEnter2D(Collision2D collision) 
